79850de286ca2d0d87eebde8b897e58398cb2879
Pipi.Y.Fu <pipifuyj@gmail.com>
2010-07-01 07:19:57 +0800

    add some comments

--- global/php/Framework/controller/Controller.php
+++ global/php/Framework/controller/Controller.php
@@ -1,13 +1,13 @@
 <?php
 class Controller{
 	public $framework=null;
-	public function index($request=null,$session=null){
+	public function index($request=null,$session=null){//index方法什么都不做
 		return true;
 	}
-	public function __call($method,$args){
+	public function __call($method,$args){//如果访问一个不存在的方法，那么默认调用index方法
 		$this->index();
 	}
-	public function __get($name){
+	public function __get($name){//重载.号方法，表示要引用的属性
 		return $this->framework->$name;
 	}
 }
--- global/php/Framework/model/Model.php
+++ global/php/Framework/model/Model.php
@@ -1,12 +1,12 @@
 <?php
 class Model{
-	public $_record="ModelRecord";
-	public $_store="ModelStore";
-	public $framework=null;
+	public $_record="ModelRecord";//私有变量，保存Model里的Record类名，以对应Record类文件，并设置默认值
+	public $_store="ModelStore";//私有变量，保存Model里的Store类名，以对应Store类文件，并设置默认值
+	public $framework=null;//framework的引用
 	public $id="";
 	public $path="";
-	public $fields=array();
-	function construct(){
+	public $fields=array();//Model的对应的域名（即：attribute）
+	function construct(){//Model对象实体化的构造函数
 		$this->Fields=array();
 		foreach($this->fields as &$field){
 			if(is_string($field))$field=new ModelField($field);
@@ -16,12 +16,14 @@ class Model{
 			}
 			$this->Fields[$field->name]=$field;
 		}
+		//判断是否有public $_record里定义的record文件，如有则保存到私有变量，_record并require进来。
 		require_once("{$this->_record}.php");
 		$path="{$this->path}/{$this->id}{$this->_record}.php";
 		if(is_file($path)){
 			require_once($path);
 			$this->_record="{$this->id}{$this->_record}";
 		}
+		//判断是否有public$_store定义的store文件
 		require_once("{$this->_store}.php");
 		$path="{$this->path}/{$this->id}{$this->_store}.php";
 		if(is_file($path)){
